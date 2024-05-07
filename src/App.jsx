9c8a904463c7f909267fb34009adf4380e9c8b01
hmmasudsarder <hmmasudsarder1@gmail.com>
2024-05-07 22:09:38 +0600

    some change

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,23 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [select, setSelect] = useState(false)
+  const [select, setSelect] = useState(false);
   return (
-    <>
-     <div onClick={() => setSelect(!select)} className={`flex w-20 h-10 bg-gray-600 m-10 rounded-full ${select ? 'bg-green-400' : ''}`}>
-      <span className={`h-10 w-10 bg-white rounded-full ${select ? 'ml-8' : ''}`}></span>
-     </div>
-    </>
+    <div className="">
+      <div
+        onClick={() => setSelect(!select)}
+        className={`flex w-20 h-10 bg-gray-600 m-12 rounded-full text-right font-bold ${
+          select ? "bg-green-200" : "bg-black text-white"
+        }`}
+      >
+        <span className="">{select ? "DARK" : "White"}</span>
+        <div
+          className={`h-10 w-10 bg-white rounded-full transition-all duration-500 cursor-pointer ${
+            select ? "-ml-10" : ""
+          }`}
+        ></div>
+      </div>
+    </div>
   );
 }
 
